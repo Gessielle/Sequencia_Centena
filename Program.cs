4691bd9ef4b44622250b62c4bc3128b9ebb2ec02
Gessielle <gessielle-barossi@hotmail.com>
2020-11-19 22:32:16 -0300

    Exiba os 100 primeiros números naturais não nulos.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sequencia_Centena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero = 0;
+            while (Numero<=100)
+        
+
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Numero = { Numero }");
+                Numero++;
+                Console.ResetColor();
+            }
+        
+                Console.WriteLine("TECLE ENTER PARA SAIR");
+                Console.ReadLine();
+                Console.WriteLine("FIM");
+                Console.ReadKey();
+                
+            
+        }
+    }
+}
